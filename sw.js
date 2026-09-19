fb2d5ea188a0c4df615e76644e06265e32b5a241
madboy27 <114769228+madboy27@users.noreply.github.com>
2026-09-19 14:51:38 +0600

    Update service worker cache version to 'hisab-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='hisab-v1',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+const C='hisab-v2',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
