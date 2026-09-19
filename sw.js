325d26f4043a4ec9809e246cc6960e0f4e141918
madboy27 <114769228+madboy27@users.noreply.github.com>
2026-09-19 14:40:55 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const C='hisab-v1',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>{
+    const f=fetch(e.request).then(n=>{const cp=n.clone();caches.open(C).then(c=>c.put(e.request,cp));return n}).catch(()=>r);
+    return r||f;
+  }));
+});
